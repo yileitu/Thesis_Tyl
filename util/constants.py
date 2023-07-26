9e0550d9d35a6e81d8f263c11c2dad478f5901a1
yileitu <yileitu@student.ethz.ch>
2023-07-26 22:51:18 +0200

    Ture/False question answering

--- util/constants.py
+++ util/constants.py
@@ -10,13 +10,15 @@ SEED: Final[int] = 21946520
 PROMPT_TEMPLATE: str = "Below is an input that describes a task or asks a problem. Write a response that appropriately completes the request.\n\n### Input:\n{input_text}\n\n### Response:\n"
 RESPONSE_SPLIT: Final[str] = "### Response:"
 
+TF_PROMPT_TEMPLATE: str = "Based on the information in: \"{passage}\", please respond to the following question with either 'TRUE' or 'FALSE': \"{question}\""
+
 # Paths
 LLM_HF_PATHS_DIR: Final[str] = "util/llm_name2hf_path.json"
 
 # Classes
 GEN_CONFIG_FOR_ALL_LLM: Final[GenerationConfig] = GenerationConfig(
-	max_length=10000,
-	max_new_tokens=10000,
+	max_length=5000,
+	max_new_tokens=5000,
 	temperature=0.1,
 	top_p=1.0,
 	top_k=10,
