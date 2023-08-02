1799c89f2a286e4fb868edcff007aae6025607c2
yileitu <yileitu@student.ethz.ch>
2023-08-02 10:34:59 +0200

    Only the response column is stored

--- chatgpt/chatgpt_gen_response.py
+++ chatgpt/chatgpt_gen_response.py
@@ -14,20 +14,28 @@ from util.util_func import MCOptions, find_first_unprocessed, gen_mc_templated_p
 # ChatGPT
 openai.api_key = os.getenv("OPENAI_API_KEY")
 LLM_NAME: str = "gpt-3.5-turbo"
-TASK = Task.TF
+TASK = Task.MC
 
 # Load the dataset
 if TASK == Task.MC:
 	DF_PATH: str = "/Users/tuyilei/Desktop/Thesis/Thesis_Tyl/data/output/mc.csv"
+	RESPONSE_PATH: str = f"/Users/tuyilei/Desktop/Thesis/Thesis_Tyl/data/output/mc_response_{LLM_NAME}.csv"
 elif TASK == Task.TF:
 	DF_PATH: str = "/Users/tuyilei/Desktop/Thesis/Thesis_Tyl/data/output/boolq.csv"
 else:
 	raise ValueError("Invalid task type")
 
 df = pd.read_csv(DF_PATH)
+num_rows = df.shape[0]
 output_col_name = f'response_{LLM_NAME}'
-if output_col_name not in df.columns:
-	df[output_col_name] = None
+# Check if the file exists, create it if not
+if not os.path.exists(RESPONSE_PATH):
+	response_df = pd.DataFrame(index=df.index, columns=[output_col_name])
+	response_df.to_csv(RESPONSE_PATH, index=False)
+	print(f"... Empty DataFrame saved to {RESPONSE_PATH}")
+else:
+	print(f"... File already exists: {RESPONSE_PATH}")
+	response_df = pd.read_csv(RESPONSE_PATH)
 
 
 def process_chatgpt_response() -> None:
@@ -35,7 +43,7 @@ def process_chatgpt_response() -> None:
 	Get response using ChatGPT API and save the responses to the dataset.
 	"""
 	# Find the first row that has not been processed
-	start_index = find_first_unprocessed(df=df, target_col_name=output_col_name)
+	start_index = find_first_unprocessed(df=response_df, target_col_name=output_col_name)
 	print(f"... Starting from index {start_index}")
 
 	# Iterate through the rows and generate responses
@@ -62,9 +70,10 @@ def process_chatgpt_response() -> None:
 				max_tokens=MAX_LEN_EXAM,
 				)
 			output_text = response["choices"][0]["message"]["content"]
-			df.loc[idx, output_col_name] = output_text
-			df.to_csv(DF_PATH, index=False)
+			response_df.loc[idx, output_col_name] = output_text
+			response_df.to_csv(RESPONSE_PATH, index=False)
 		except openai.error.APIError as e:
+			response_df.to_csv(RESPONSE_PATH, index=False)
 			print(f"... Error: {e}")
 			print(f"... Stopping at index {idx}")
 			print("... Restarting from the beginning.")
@@ -72,3 +81,4 @@ def process_chatgpt_response() -> None:
 
 
 process_chatgpt_response()
+response_df.to_csv(RESPONSE_PATH, index=False)
