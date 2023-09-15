dd0fda279fec5d5af9370168b36ec1cac9735718
yileitu <yileitu@student.ethz.ch>
2023-09-15 17:44:00 +0200

    Filtered/unfiltered ground truth stat MC

--- data_processing/mc_dataset_stat.py
+++ data_processing/mc_dataset_stat.py
@@ -3,21 +3,17 @@ import pandas as pd
 from constants_for_clean import LLM_NAMES, OPTION_E_DATASETS
 
 TASK_NAME = 'mc'
-mc_data = pd.read_csv('../data/output/mc/mc.csv')
+FILTERED: bool = True
+ORIGINAL: bool = False
 
-# 1. Check if Option E is empty
-groups = mc_data.groupby(['data_source', 'subject'])
-
-nonempty_option_e_list = []
-empty_option_e_list = []
-for name, group in groups:
-	if group['option_E'].notna().any():
-		nonempty_option_e_list.append(name)
+if ORIGINAL:
+	data_path = f'../data/output/mc/mc.csv'
+else:
+	if FILTERED:
+		data_path = f'../data/processed/{TASK_NAME}/filtered/{TASK_NAME}_filtered_answers.csv'
 	else:
-		empty_option_e_list.append(name)
-
-print(f'Nonempty Option E List: {nonempty_option_e_list}')
-print(f'Empty Option E lList: {empty_option_e_list}')
+		data_path = f'../data/processed/{TASK_NAME}/unfiltered/{TASK_NAME}_unfiltered_answers.csv'
+mc_data = pd.read_csv(data_path)
 
 # 2. Proportion of ground truth answers
 df_with_e = mc_data[mc_data.apply(lambda x: (x['data_source'], x['subject']) in OPTION_E_DATASETS, axis=1)]
@@ -36,19 +32,27 @@ gt_stat_df = pd.DataFrame(
 	)
 
 # Calculate ground truth proportions for the group with Option E
-counts_gt_with_e = df_with_e['answer'].value_counts(normalize=True)
+gt_col_name = 'answer' if ORIGINAL else 'answer_ground_truth'
+counts_gt_with_e = df_with_e[gt_col_name].value_counts(normalize=True)
 for letter in 'ABCDE':
 	gt_stat_df.loc[gt_stat_df['group'] == 'with_E', f'prop_gt_{letter}'] = counts_gt_with_e.get(letter, 0)
 
 # Calculate ground truth proportions for the group without Option E
-counts_gt_without_e = df_without_e['answer'].value_counts(normalize=True)
+counts_gt_without_e = df_without_e[gt_col_name].value_counts(normalize=True)
 for letter in 'ABCD':  # No 'E' in this group
 	gt_stat_df.loc[gt_stat_df['group'] == 'without_E', f'prop_gt_{letter}'] = counts_gt_without_e.get(letter, 0)
 
-gt_stat_df.to_csv(f'../data/processed/{TASK_NAME}/{TASK_NAME}_gt_stats.csv', index=False)
+if ORIGINAL:
+	gt_stat_save_path = f'../data/processed/{TASK_NAME}/{TASK_NAME}_gt_stats.csv'
+else:
+	if FILTERED:
+		gt_stat_save_path = f'../data/processed/{TASK_NAME}/filtered/{TASK_NAME}_filtered_gt_stats.csv'
+	else:
+		gt_stat_save_path = f'../data/processed/{TASK_NAME}/unfiltered/{TASK_NAME}_unfiltered_gt_stats.csv'
+gt_stat_df.to_csv(gt_stat_save_path, index=False)
 
 # 3. Majority Accuracy
-letter_percentages = mc_data['answer'].value_counts(normalize=True)
+letter_percentages = mc_data[gt_col_name].value_counts(normalize=True)
 max_percentage = letter_percentages.max()
 max_letter = letter_percentages.idxmax()
 
@@ -69,4 +73,11 @@ majority = pd.DataFrame(
 		'majority_letter_without_E'  : [max_letter_without_e],
 		}
 	)
-majority.to_csv(f'../data/processed/{TASK_NAME}/{TASK_NAME}_majority.csv', index=False)
+if ORIGINAL:
+	majority_save_path = f'../data/processed/{TASK_NAME}/{TASK_NAME}_majority.csv'
+else:
+	if FILTERED:
+		majority_save_path = f'../data/processed/{TASK_NAME}/filtered/{TASK_NAME}_filtered_majority.csv'
+	else:
+		majority_save_path = f'../data/processed/{TASK_NAME}/unfiltered/{TASK_NAME}_unfiltered_majority.csv'
+majority.to_csv(majority_save_path, index=False)
