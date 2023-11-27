a6c995c3f595dbb639fc24c18c68c87b62b8e435
yileitu <yileitu@student.ethz.ch>
2023-11-27 19:14:47 +0100

    delete GPTScore, update MC template

--- finetune/fill_template.py
+++ finetune/fill_template.py
@@ -6,6 +6,11 @@ from sklearn.model_selection import train_test_split
 
 from util.util_func import read_txt, save_df_to_csv
 
+# Constants
+SEED = 42
+TEST_RATIO = 0.2
+DEV_RATIO = TEST_RATIO / (1 - TEST_RATIO)  # 0.25 x 0.8 = 0.2
+
 # Define file paths
 save_dir = '../data/templated/mc'
 df_labeled_path = '../data/clean/mc_labeled.csv'
@@ -47,7 +52,7 @@ def fill_template(row: pd.DataFrame) -> str:
 
 	return template.format(
 		passage=row.get('passage', ''),
-		question=row.get('passage', ''),
+		question=row.get('question', ''),
 		option_A=row.get('option_A', ''),
 		option_B=row.get('option_B', ''),
 		option_C=row.get('option_C', ''),
@@ -56,16 +61,34 @@ def fill_template(row: pd.DataFrame) -> str:
 		)
 
 
+def preprocess_data(df):
+	# Combine the 'filled_template' and 'label' into a single string
+	df['input_target'] = df['filled_template'] + " <label> " + df['label']
+	return df['input_target'].tolist()
+
+
 df['filled_template'] = df.apply(fill_template, axis=1)
 fine_tune_df = df[['id', 'filled_template', 'label']]
 
 # Split the data into train+dev and test sets
-train_dev_df, test_df = train_test_split(fine_tune_df, test_size=0.2, random_state=42)
+train_dev_df, test_df = train_test_split(fine_tune_df, test_size=TEST_RATIO, random_state=SEED)
 
 # Split the train+dev set into train and dev sets
-train_df, dev_df = train_test_split(train_dev_df, test_size=0.25, random_state=42)  # 0.25 x 0.8 = 0.2
+train_df, dev_df = train_test_split(train_dev_df, test_size=DEV_RATIO, random_state=SEED)  # 0.25 x 0.8 = 0.2
 
 # Save these splits to new CSV files
 save_df_to_csv(df=train_df, path=os.path.join(save_dir, 'train_data.csv'), index=False)
 save_df_to_csv(df=dev_df, path=os.path.join(save_dir, 'dev_data.csv'), index=False)
 save_df_to_csv(df=test_df, path=os.path.join(save_dir, 'test_data.csv'), index=False)
+
+train_texts = preprocess_data(train_df)
+dev_texts = preprocess_data(dev_df)
+test_texts = preprocess_data(test_df)
+
+# Save texts to files
+with open(os.path.join(save_dir, 'train_texts.txt'), 'w') as f:
+	f.write("<|endoftext|>\n".join(train_texts))
+with open(os.path.join(save_dir, 'dev_texts.txt'), 'w') as f:
+	f.write("<|endoftext|>\n".join(dev_texts))
+with open(os.path.join(save_dir, 'test_texts.txt'), 'w') as f:
+	f.write("<|endoftext|>\n".join(test_texts))
--- finetune/finetune.py
+++ finetune/finetune.py
@@ -1,28 +1,61 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
-
-from util.struct import Task
-
-TASK: Task = Task.MC
-
-if TASK == Task.MC:
-	df_labeled_path = '../data/clean/mc_labeled.csv'
-	template = '/prompt_template/mc_template_with_passage_and_option_e.txt'
-
-df = pd.read_csv(df_labeled_path)
+from transformers import DataCollatorForLanguageModeling, GPT2LMHeadModel, GPT2Tokenizer, TextDataset, Trainer, \
+	TrainingArguments
 
 
-# Function to fill in the template with values from a row
-def fill_template(row, template):
-	return template.format(
-		passage=row['passage'],
-		question=row['question'],
-		option_A=row['option_A'],
-		option_B=row['option_B'],
-		option_C=row['option_C'],
-		option_D=row['option_D'],
-		option_E=row['option_E']
-		)
+# Load and preprocess the data
+def preprocess_data(df):
+	# Combine the 'filled_template' and 'label' into a single string
+	df['input_target'] = df['filled_template'] + " <label> " + df['label']
+	return df['input_target'].tolist()
 
 
-df['filled_template'] = df.apply(fill_template, axis=1, args=(template,))
+# Prepare datasets
+train_df = pd.read_csv('../data/templated/mc/train_data.csv')
+dev_df = pd.read_csv('../data/templated/mc/dev_data.csv')
+test_df = pd.read_csv('../data/templated/mc/test_data.csv')
+
+train_texts = preprocess_data(train_df)
+dev_texts = preprocess_data(dev_df)
+test_texts = preprocess_data(test_df)
+
+# Save preprocessed texts to files (required by TextDataset)
+with open('train_texts.txt', 'w') as f:
+	f.write("\n".join(train_texts))
+
+# Initialize the tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+# Special tokens to add (if any)
+special_tokens_dict = {'additional_special_tokens': ['<label>']}
+tokenizer.add_special_tokens(special_tokens_dict)
+model.resize_token_embeddings(len(tokenizer))
+
+# Prepare datasets for training
+train_dataset = TextDataset(tokenizer=tokenizer, file_path='train_texts.txt', block_size=128)
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+# Training arguments
+training_args = TrainingArguments(
+	output_dir='./results',
+	num_train_epochs=3,
+	per_device_train_batch_size=2,
+	save_steps=10_000,
+	save_total_limit=2,
+	)
+
+# Initialize Trainer
+trainer = Trainer(
+	model=model,
+	args=training_args,
+	data_collator=data_collator,
+	train_dataset=train_dataset,
+	)
+
+# Train the model
+trainer.train()
+
+# Save the model
+model.save_pretrained('./finetuned_model')
